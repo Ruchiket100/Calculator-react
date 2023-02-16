efda4624799ce0f372ed3b64c52ee1b0662899ba
Ruchiket100 <RuchiketBorse1@gmail.com>
2023-02-16 21:56:48 +0530

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import Display from "./components/Display"
+import "./App.css"
+
+function App() {
+    // Display to show the input
+    // Keyboard with buttons with unique value
+
+    return (
+        <div className="App">
+            {/* Display */}
+            <Display />
+        </div>
+    )
+}
+
+export default App
